c57e65eeb78b267448a9bd6281aef8ada7df4095
Vitor Bionic <vitorbionic@hotmail.com>
2025-05-09 19:20:34 -0300

    first commit

---
+++ retail-analysis-api/src/main/java/com/vitorbionic/RetailAnalysisApiApplication.java
@@ -0,0 +1,13 @@
+package com.vitorbionic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetailAnalysisApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetailAnalysisApiApplication.class, args);
+	}
+
+}
---
+++ retail-analysis-api/src/test/java/com/vitorbionic/RetailAnalysisApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vitorbionic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetailAnalysisApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
