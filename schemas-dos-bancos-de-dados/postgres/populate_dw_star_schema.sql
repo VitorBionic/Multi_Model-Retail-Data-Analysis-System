71cfc8826da98cbeae371c98e77da95b92e29168
Vitor Bionic <vitorbionic@hotmail.com>
2025-05-27 14:30:06 -0300

    submit the assignment

---
+++ schemas-dos-bancos-de-dados/postgres/populate_dw_star_schema.sql
@@ -0,0 +1,220 @@
+INSERT INTO dim_client (id, name, email, city, state) VALUES (1, 'Davi Lucas Lima', 'nascimentoluiz-fernando@bol.com.br', 'Recife', 'RJ');
+INSERT INTO dim_client (id, name, email, city, state) VALUES (2, 'Marcos Vinicius Cardoso', 'ryanda-paz@freitas.net', 'Curitiba', 'SP');
+INSERT INTO dim_client (id, name, email, city, state) VALUES (3, 'Luiz Otávio Moraes', 'souzaemanuel@vieira.br', 'São Paulo', 'RJ');
+INSERT INTO dim_client (id, name, email, city, state) VALUES (4, 'Sra. Catarina Costela', 'azevedoyasmin@ig.com.br', 'Recife', 'PR');
+INSERT INTO dim_client (id, name, email, city, state) VALUES (5, 'Bryan Castro', 'davi-luiz78@hotmail.com', 'Recife', 'MG');
+INSERT INTO dim_client (id, name, email, city, state) VALUES (6, 'Caio Moreira', 'da-motavinicius@gmail.com', 'Recife', 'RJ');
+INSERT INTO dim_client (id, name, email, city, state) VALUES (7, 'Theo Caldeira', 'omoraes@gmail.com', 'São Paulo', 'RJ');
+INSERT INTO dim_client (id, name, email, city, state) VALUES (8, 'Vitor Nunes', 'barrosluiz-gustavo@gmail.com', 'Recife', 'PR');
+INSERT INTO dim_client (id, name, email, city, state) VALUES (9, 'Dra. Lorena Melo', 'yda-luz@jesus.br', 'Belo Horizonte', 'MG');
+INSERT INTO dim_client (id, name, email, city, state) VALUES (10, 'Luiz Otávio Pires', 'maria-fernandapereira@araujo.com', 'Rio de Janeiro', 'MG');
+INSERT INTO dim_client (id, name, email, city, state) VALUES (11, 'Vitor Hugo Pinto', 'noah62@hotmail.com', 'Belo Horizonte', 'PE');
+INSERT INTO dim_client (id, name, email, city, state) VALUES (12, 'Theo Novaes', 'correiadavi-lucca@bol.com.br', 'Rio de Janeiro', 'PE');
+INSERT INTO dim_client (id, name, email, city, state) VALUES (13, 'Nicolas Moura', 'rezendepedro-henrique@uol.com.br', 'Rio de Janeiro', 'RJ');
+INSERT INTO dim_client (id, name, email, city, state) VALUES (14, 'Dr. Henrique Aragão', 'ana47@bol.com.br', 'Curitiba', 'PE');
+INSERT INTO dim_client (id, name, email, city, state) VALUES (15, 'Vitória Pires', 'melodavi-lucas@almeida.br', 'Rio de Janeiro', 'MG');
+INSERT INTO dim_client (id, name, email, city, state) VALUES (16, 'Sr. Enzo Jesus', 'blima@pires.com', 'São Paulo', 'PR');
+INSERT INTO dim_client (id, name, email, city, state) VALUES (17, 'Sr. Gabriel da Rocha', 'costelamaria-clara@campos.com', 'Belo Horizonte', 'MG');
+INSERT INTO dim_client (id, name, email, city, state) VALUES (18, 'Henrique Carvalho', 'vianastella@dias.org', 'São Paulo', 'MG');
+INSERT INTO dim_client (id, name, email, city, state) VALUES (19, 'Yasmin Nascimento', 'ccunha@almeida.br', 'São Paulo', 'PR');
+INSERT INTO dim_client (id, name, email, city, state) VALUES (20, 'Bruna Fogaça', 'maysacavalcanti@hotmail.com', 'Curitiba', 'RJ');
+INSERT INTO dim_film (id, title, genre, duration) VALUES (1, 'Ipsam', 'Drama', 100);
+INSERT INTO dim_film (id, title, genre, duration) VALUES (2, 'Expedita a quam', 'Romance', 120);
+INSERT INTO dim_film (id, title, genre, duration) VALUES (3, 'Animi repellendus provident', 'Romance', 115);
+INSERT INTO dim_film (id, title, genre, duration) VALUES (4, 'Perspiciatis possimus', 'Ação', 151);
+INSERT INTO dim_film (id, title, genre, duration) VALUES (5, 'Quod ea', 'Romance', 136);
+INSERT INTO dim_film (id, title, genre, duration) VALUES (6, 'Quo quis deleniti', 'Drama', 123);
+INSERT INTO dim_film (id, title, genre, duration) VALUES (7, 'Cupiditate ab', 'Ação', 115);
+INSERT INTO dim_film (id, title, genre, duration) VALUES (8, 'Possimus a', 'Ficção', 139);
+INSERT INTO dim_film (id, title, genre, duration) VALUES (9, 'Voluptatibus libero vitae', 'Romance', 157);
+INSERT INTO dim_film (id, title, genre, duration) VALUES (10, 'Est qui ipsum', 'Animação', 134);
+INSERT INTO dim_film (id, title, genre, duration) VALUES (11, 'Fugit aut', 'Animação', 129);
+INSERT INTO dim_film (id, title, genre, duration) VALUES (12, 'Corrupti', 'Ficção', 134);
+INSERT INTO dim_film (id, title, genre, duration) VALUES (13, 'Consequuntur doloremque', 'Ação', 120);
+INSERT INTO dim_film (id, title, genre, duration) VALUES (14, 'Consectetur vitae commodi', 'Comédia', 112);
+INSERT INTO dim_film (id, title, genre, duration) VALUES (15, 'Fugiat corrupti', 'Drama', 90);
+INSERT INTO dim_film (id, title, genre, duration) VALUES (16, 'Ducimus', 'Drama', 165);
+INSERT INTO dim_film (id, title, genre, duration) VALUES (17, 'Facere ipsa', 'Ação', 128);
+INSERT INTO dim_film (id, title, genre, duration) VALUES (18, 'Vel dignissimos facilis', 'Animação', 140);
+INSERT INTO dim_film (id, title, genre, duration) VALUES (19, 'Fugit cumque labore', 'Romance', 114);
+INSERT INTO dim_film (id, title, genre, duration) VALUES (20, 'Blanditiis', 'Terror', 171);
+INSERT INTO dim_store (id, name, city, state) VALUES (1, 'Store C', 'São Paulo', 'RJ');
+INSERT INTO dim_store (id, name, city, state) VALUES (2, 'Store A', 'Rio de Janeiro', 'RJ');
+INSERT INTO dim_store (id, name, city, state) VALUES (3, 'Store D', 'São Paulo', 'PE');
+INSERT INTO dim_store (id, name, city, state) VALUES (4, 'Store D', 'Belo Horizonte', 'RJ');
+INSERT INTO dim_store (id, name, city, state) VALUES (5, 'Store A', 'Belo Horizonte', 'MG');
+INSERT INTO dim_store (id, name, city, state) VALUES (6, 'Store B', 'Belo Horizonte', 'PE');
+INSERT INTO dim_store (id, name, city, state) VALUES (7, 'Store C', 'Belo Horizonte', 'SP');
+INSERT INTO dim_store (id, name, city, state) VALUES (8, 'Store D', 'Rio de Janeiro', 'PR');
+INSERT INTO dim_store (id, name, city, state) VALUES (9, 'Store B', 'Belo Horizonte', 'PE');
+INSERT INTO dim_store (id, name, city, state) VALUES (10, 'Store B', 'São Paulo', 'RJ');
+INSERT INTO dim_store (id, name, city, state) VALUES (11, 'Store A', 'Rio de Janeiro', 'SP');
+INSERT INTO dim_store (id, name, city, state) VALUES (12, 'Store C', 'São Paulo', 'RJ');
+INSERT INTO dim_store (id, name, city, state) VALUES (13, 'Store C', 'Belo Horizonte', 'RJ');
+INSERT INTO dim_store (id, name, city, state) VALUES (14, 'Store D', 'Curitiba', 'PR');
+INSERT INTO dim_store (id, name, city, state) VALUES (15, 'Store A', 'Rio de Janeiro', 'MG');
+INSERT INTO dim_store (id, name, city, state) VALUES (16, 'Store B', 'Belo Horizonte', 'PE');
+INSERT INTO dim_store (id, name, city, state) VALUES (17, 'Store B', 'Recife', 'SP');
+INSERT INTO dim_store (id, name, city, state) VALUES (18, 'Store B', 'São Paulo', 'SP');
+INSERT INTO dim_store (id, name, city, state) VALUES (19, 'Store C', 'Belo Horizonte', 'RJ');
+INSERT INTO dim_store (id, name, city, state) VALUES (20, 'Store D', 'São Paulo', 'PR');
+INSERT INTO dim_time (id, year, month, day, date) VALUES (1, 2022, 1, 8, '2022-01-08');
+INSERT INTO dim_time (id, year, month, day, date) VALUES (2, 2022, 1, 15, '2022-01-15');
+INSERT INTO dim_time (id, year, month, day, date) VALUES (3, 2022, 1, 22, '2022-01-22');
+INSERT INTO dim_time (id, year, month, day, date) VALUES (4, 2022, 1, 29, '2022-01-29');
+INSERT INTO dim_time (id, year, month, day, date) VALUES (5, 2022, 2, 5, '2022-02-05');
+INSERT INTO dim_time (id, year, month, day, date) VALUES (6, 2022, 2, 12, '2022-02-12');
+INSERT INTO dim_time (id, year, month, day, date) VALUES (7, 2022, 2, 19, '2022-02-19');
+INSERT INTO dim_time (id, year, month, day, date) VALUES (8, 2022, 2, 26, '2022-02-26');
+INSERT INTO dim_time (id, year, month, day, date) VALUES (9, 2022, 3, 5, '2022-03-05');
+INSERT INTO dim_time (id, year, month, day, date) VALUES (10, 2022, 3, 12, '2022-03-12');
+INSERT INTO dim_time (id, year, month, day, date) VALUES (11, 2022, 3, 19, '2022-03-19');
+INSERT INTO dim_time (id, year, month, day, date) VALUES (12, 2022, 3, 26, '2022-03-26');
+INSERT INTO dim_time (id, year, month, day, date) VALUES (13, 2022, 4, 2, '2022-04-02');
+INSERT INTO dim_time (id, year, month, day, date) VALUES (14, 2022, 4, 9, '2022-04-09');
+INSERT INTO dim_time (id, year, month, day, date) VALUES (15, 2022, 4, 16, '2022-04-16');
+INSERT INTO dim_time (id, year, month, day, date) VALUES (16, 2022, 4, 23, '2022-04-23');
+INSERT INTO dim_time (id, year, month, day, date) VALUES (17, 2022, 4, 30, '2022-04-30');
+INSERT INTO dim_time (id, year, month, day, date) VALUES (18, 2022, 5, 7, '2022-05-07');
+INSERT INTO dim_time (id, year, month, day, date) VALUES (19, 2022, 5, 14, '2022-05-14');
+INSERT INTO dim_time (id, year, month, day, date) VALUES (20, 2022, 5, 21, '2022-05-21');
+INSERT INTO fact_rental (id, time_id, film_id, client_id, store_id, paidValue, quantity)
+                       VALUES (1, 14, 2, 10,
+                               6, 43.32, 2);
+INSERT INTO fact_rental (id, time_id, film_id, client_id, store_id, paidValue, quantity)
+                       VALUES (2, 10, 10, 2,
+                               9, 20.24, 2);
+INSERT INTO fact_rental (id, time_id, film_id, client_id, store_id, paidValue, quantity)
+                       VALUES (3, 8, 14, 2,
+                               5, 10.61, 5);
+INSERT INTO fact_rental (id, time_id, film_id, client_id, store_id, paidValue, quantity)
+                       VALUES (4, 7, 10, 1,
+                               2, 20.5, 2);
+INSERT INTO fact_rental (id, time_id, film_id, client_id, store_id, paidValue, quantity)
+                       VALUES (5, 20, 7, 20,
+                               18, 20.88, 4);
+INSERT INTO fact_rental (id, time_id, film_id, client_id, store_id, paidValue, quantity)
+                       VALUES (6, 8, 11, 19,
+                               2, 35.69, 4);
+INSERT INTO fact_rental (id, time_id, film_id, client_id, store_id, paidValue, quantity)
+                       VALUES (7, 11, 15, 16,
+                               13, 29.66, 3);
+INSERT INTO fact_rental (id, time_id, film_id, client_id, store_id, paidValue, quantity)
+                       VALUES (8, 3, 17, 11,
+                               19, 27.61, 1);
+INSERT INTO fact_rental (id, time_id, film_id, client_id, store_id, paidValue, quantity)
+                       VALUES (9, 17, 13, 16,
+                               13, 14.15, 1);
+INSERT INTO fact_rental (id, time_id, film_id, client_id, store_id, paidValue, quantity)
+                       VALUES (10, 6, 2, 13,
+                               14, 31.79, 4);
+INSERT INTO fact_rental (id, time_id, film_id, client_id, store_id, paidValue, quantity)
+                       VALUES (11, 18, 11, 10,
+                               13, 27.47, 5);
+INSERT INTO fact_rental (id, time_id, film_id, client_id, store_id, paidValue, quantity)
+                       VALUES (12, 10, 8, 11,
+                               9, 14.72, 2);
+INSERT INTO fact_rental (id, time_id, film_id, client_id, store_id, paidValue, quantity)
+                       VALUES (13, 19, 6, 5,
+                               4, 24.81, 4);
+INSERT INTO fact_rental (id, time_id, film_id, client_id, store_id, paidValue, quantity)
+                       VALUES (14, 11, 8, 13,
+                               7, 12.22, 1);
+INSERT INTO fact_rental (id, time_id, film_id, client_id, store_id, paidValue, quantity)
+                       VALUES (15, 9, 2, 12,
+                               8, 24.09, 5);
+INSERT INTO fact_rental (id, time_id, film_id, client_id, store_id, paidValue, quantity)
+                       VALUES (16, 15, 18, 3,
+                               19, 28.23, 2);
+INSERT INTO fact_rental (id, time_id, film_id, client_id, store_id, paidValue, quantity)
+                       VALUES (17, 1, 4, 17,
+                               10, 10.28, 4);
+INSERT INTO fact_rental (id, time_id, film_id, client_id, store_id, paidValue, quantity)
+                       VALUES (18, 3, 20, 8,
+                               3, 24.8, 5);
+INSERT INTO fact_rental (id, time_id, film_id, client_id, store_id, paidValue, quantity)
+                       VALUES (19, 1, 1, 6,
+                               6, 40.07, 3);
+INSERT INTO fact_rental (id, time_id, film_id, client_id, store_id, paidValue, quantity)
+                       VALUES (20, 20, 15, 20,
+                               10, 46.14, 2);
+INSERT INTO stock_temporal (id, film_id, quantity, start_validity, end_validity)
+                       VALUES (1, 1, 25, '2023-01-06', '2023-04-06');
+INSERT INTO stock_temporal (id, film_id, quantity, start_validity, end_validity)
+                       VALUES (2, 2, 95, '2023-01-11', '2023-04-11');
+INSERT INTO stock_temporal (id, film_id, quantity, start_validity, end_validity)
+                       VALUES (3, 3, 91, '2023-01-16', '2023-04-16');
+INSERT INTO stock_temporal (id, film_id, quantity, start_validity, end_validity)
+                       VALUES (4, 4, 65, '2023-01-21', '2023-04-21');
+INSERT INTO stock_temporal (id, film_id, quantity, start_validity, end_validity)
+                       VALUES (5, 5, 99, '2023-01-26', '2023-04-26');
+INSERT INTO stock_temporal (id, film_id, quantity, start_validity, end_validity)
+                       VALUES (6, 6, 63, '2023-01-31', '2023-05-01');
+INSERT INTO stock_temporal (id, film_id, quantity, start_validity, end_validity)
+                       VALUES (7, 7, 68, '2023-02-05', '2023-05-06');
+INSERT INTO stock_temporal (id, film_id, quantity, start_validity, end_validity)
+                       VALUES (8, 8, 38, '2023-02-10', '2023-05-11');
+INSERT INTO stock_temporal (id, film_id, quantity, start_validity, end_validity)
+                       VALUES (9, 9, 40, '2023-02-15', '2023-05-16');
+INSERT INTO stock_temporal (id, film_id, quantity, start_validity, end_validity)
+                       VALUES (10, 10, 30, '2023-02-20', '2023-05-21');
+INSERT INTO stock_temporal (id, film_id, quantity, start_validity, end_validity)
+                       VALUES (11, 11, 10, '2023-02-25', '2023-05-26');
+INSERT INTO stock_temporal (id, film_id, quantity, start_validity, end_validity)
+                       VALUES (12, 12, 58, '2023-03-02', '2023-05-31');
+INSERT INTO stock_temporal (id, film_id, quantity, start_validity, end_validity)
+                       VALUES (13, 13, 55, '2023-03-07', '2023-06-05');
+INSERT INTO stock_temporal (id, film_id, quantity, start_validity, end_validity)
+                       VALUES (14, 14, 51, '2023-03-12', '2023-06-10');
+INSERT INTO stock_temporal (id, film_id, quantity, start_validity, end_validity)
+                       VALUES (15, 15, 15, '2023-03-17', '2023-06-15');
+INSERT INTO stock_temporal (id, film_id, quantity, start_validity, end_validity)
+                       VALUES (16, 16, 29, '2023-03-22', '2023-06-20');
+INSERT INTO stock_temporal (id, film_id, quantity, start_validity, end_validity)
+                       VALUES (17, 17, 25, '2023-03-27', '2023-06-25');
+INSERT INTO stock_temporal (id, film_id, quantity, start_validity, end_validity)
+                       VALUES (18, 18, 51, '2023-04-01', '2023-06-30');
+INSERT INTO stock_temporal (id, film_id, quantity, start_validity, end_validity)
+                       VALUES (19, 19, 23, '2023-04-06', '2023-07-05');
+INSERT INTO stock_temporal (id, film_id, quantity, start_validity, end_validity)
+                       VALUES (20, 20, 42, '2023-04-11', '2023-07-10');
+INSERT INTO price_temporal (id, film_id, price, start_validity, end_validity)
+                       VALUES (1, 1, 14.85, '2023-01-04', '2023-03-05');
+INSERT INTO price_temporal (id, film_id, price, start_validity, end_validity)
+                       VALUES (2, 2, 8.4, '2023-01-07', '2023-03-08');
+INSERT INTO price_temporal (id, film_id, price, start_validity, end_validity)
+                       VALUES (3, 3, 9.27, '2023-01-10', '2023-03-11');
+INSERT INTO price_temporal (id, film_id, price, start_validity, end_validity)
+                       VALUES (4, 4, 13.48, '2023-01-13', '2023-03-14');
+INSERT INTO price_temporal (id, film_id, price, start_validity, end_validity)
+                       VALUES (5, 5, 9.16, '2023-01-16', '2023-03-17');
+INSERT INTO price_temporal (id, film_id, price, start_validity, end_validity)
+                       VALUES (6, 6, 10.1, '2023-01-19', '2023-03-20');
+INSERT INTO price_temporal (id, film_id, price, start_validity, end_validity)
+                       VALUES (7, 7, 18.07, '2023-01-22', '2023-03-23');
+INSERT INTO price_temporal (id, film_id, price, start_validity, end_validity)
+                       VALUES (8, 8, 19.24, '2023-01-25', '2023-03-26');
+INSERT INTO price_temporal (id, film_id, price, start_validity, end_validity)
+                       VALUES (9, 9, 8.2, '2023-01-28', '2023-03-29');
+INSERT INTO price_temporal (id, film_id, price, start_validity, end_validity)
+                       VALUES (10, 10, 13.43, '2023-01-31', '2023-04-01');
+INSERT INTO price_temporal (id, film_id, price, start_validity, end_validity)
+                       VALUES (11, 11, 10.43, '2023-02-03', '2023-04-04');
+INSERT INTO price_temporal (id, film_id, price, start_validity, end_validity)
+                       VALUES (12, 12, 17.05, '2023-02-06', '2023-04-07');
+INSERT INTO price_temporal (id, film_id, price, start_validity, end_validity)
+                       VALUES (13, 13, 17.89, '2023-02-09', '2023-04-10');
+INSERT INTO price_temporal (id, film_id, price, start_validity, end_validity)
+                       VALUES (14, 14, 15.99, '2023-02-12', '2023-04-13');
+INSERT INTO price_temporal (id, film_id, price, start_validity, end_validity)
+                       VALUES (15, 15, 15.04, '2023-02-15', '2023-04-16');
+INSERT INTO price_temporal (id, film_id, price, start_validity, end_validity)
+                       VALUES (16, 16, 12.42, '2023-02-18', '2023-04-19');
+INSERT INTO price_temporal (id, film_id, price, start_validity, end_validity)
+                       VALUES (17, 17, 12.94, '2023-02-21', '2023-04-22');
+INSERT INTO price_temporal (id, film_id, price, start_validity, end_validity)
+                       VALUES (18, 18, 13.33, '2023-02-24', '2023-04-25');
+INSERT INTO price_temporal (id, film_id, price, start_validity, end_validity)
+                       VALUES (19, 19, 15.15, '2023-02-27', '2023-04-28');
+INSERT INTO price_temporal (id, film_id, price, start_validity, end_validity)
+                       VALUES (20, 20, 15.44, '2023-03-02', '2023-05-01');
